0160cf3922871fc209904a5c65c105fcfa21dd3b
Miguel Cardenas <c.miguel707@gmail.com>
2015-10-17 16:36:14 -0500

    added createAGraph which creates a graph

--- gowiththeflow-project/src/test/TestDriver.java
+++ gowiththeflow-project/src/test/TestDriver.java
@@ -1,12 +1,27 @@
 package test;
+import GUI.*;
+import javax.swing.*;
 
 public class TestDriver {
 
 	public static void main(String[] args) throws Exception {
 		
 		Reader test = new Reader("data/sciences_computerscience.txt");
-
-		test.read();
+		
+		//String[] courseList = test.read();
+		//test.read();
+		
+		String[] courseList = {"course1", "course2", "course3", "course4", "course5"};
+		
+		FlowModel model = new FlowModel();
+		FlowView view = new FlowView(model, courseList);
+		FlowController controller = new FlowController(model, view);
+		
+		view.registerListener(controller);
+		view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		view.setSize(800, 500);
+		view.setVisible(true);
 	}
+	
 
 }
