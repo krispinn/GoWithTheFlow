5b1596e39a9de7221b0535a1bc687445792bab48
Danny Tsang <d87tsang@gmail.com>
2015-10-17 15:37:26 -0500

    included a test package to test jgraphx functionality

---
+++ gowiththeflow-project/src/test/TestDriver.java
@@ -0,0 +1,12 @@
+package test;
+
+public class TestDriver {
+
+	public static void main(String[] args) throws Exception {
+		
+		Reader test = new Reader("data/sciences_computerscience.txt");
+
+		test.read();
+	}
+
+}
