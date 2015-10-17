b76d41203098291ba1911d71d14cf4c342b9a00b
Danny Tsang <d87tsang@gmail.com>
2015-10-17 17:51:39 -0500

    getting the gui up!

--- gowiththeflow-project/src/test/TestDriver.java
+++ gowiththeflow-project/src/test/TestDriver.java
@@ -1,30 +1,40 @@
 package test;
 import GUI.*;
+import gowiththeflow.CourseList;
+
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class TestDriver {
 
 	public static void main(String[] args) throws Exception {
 		
-		Reader test = new Reader("data/liberalfinearts_music.txt");
+		Reader test = new Reader("data/sciences_computerscience.txt");
 
-		test.read();
 		//Reader test = new Reader("data/sciences_computerscience.txt");
 		
-		//String[] courseList = test.read();
-		//test.read();
+		ArrayList<String[]> temp = test.read();
 		
-		String[] courseList = {"course1", "course2", "course3", "course4", "course5"};
+		String[] courseList = new String[126];
+		
+		int i = 0;
+		
+		for (String[] s : temp) {
+			courseList[i++] = s[2];
+
+		}		
+		
 		
 		FlowModel model = new FlowModel();
 		FlowView view = new FlowView(model, courseList);
+		
 		FlowController controller = new FlowController(model, view);
 		
 		view.registerListener(controller);
 		view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		view.setSize(800, 500);
 		view.setVisible(true);
-
+		
 	}
 	
 
