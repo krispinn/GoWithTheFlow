3e47c181a4777be91d3693ee7723cb11f75079ca
Danny Tsang <d87tsang@gmail.com>
2015-10-28 14:32:40 -0500

    Adding nodes

---
+++ gowiththeflow-project/src/testdannybilal/DirectedEdge.java
@@ -0,0 +1,12 @@
+package testdannybilal;
+
+public abstract class DirectedEdge {
+	/*protected final Node endPoint1;
+	
+	public DirectedEdge(Node endPoint) {
+		endPoint1 = endPoint;
+	}
+	public Node getEndPoint1() {
+		return endPoint1;
+	}*/
+}
