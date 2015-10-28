64d90a493e2095edb36d5a30313a0ff9d20534b7
Bilal Siddiqui <sl-matrix@hotmail.com>
2015-10-28 14:48:51 -0500

    Deleted old data

--- gowiththeflow-project/src/testdannybilal/TestDriver.java
+++ gowiththeflow-project/src/testdannybilal/TestDriver.java
@@ -17,6 +17,7 @@ public class TestDriver {
 		//Reader test = new Reader("data/sciences_computerscience.txt");
 		
 		ArrayList<Course> temp = test.read();	
+		System.out.println(temp);
 		Graph g = new Graph(temp);
 		//System.out.println(temp);
 		
