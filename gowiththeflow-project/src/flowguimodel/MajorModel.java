b138418c9d9c4fbe301121e349b1aac6c93341e9
Jason Blig <bligjason@gmail.com>
2015-11-17 16:34:23 -0600

    added "Laboratory" line to MajorModel.java

--- gowiththeflow-project/src/flowguimodel/MajorModel.java
+++ gowiththeflow-project/src/flowguimodel/MajorModel.java
@@ -64,7 +64,7 @@ public class MajorModel {
 			String courseDescription = tokens[6].trim();
 			//this is where the checkbox affects the flowchart
 			if(this.recitationCheck == 0) {
-				if(!(courseName.endsWith("Recitation"))){
+				if((!(courseName.endsWith("Recitation"))) || (!(courseName.endsWith("Laboratory")))){
 					CourseModel course = new CourseModel(subject, courseNumber, courseName, prerequisiteCourses,
 							concurrentEnrollment, courseDescription, courseEnrollmentInfo, num++);
 					listOfCourses.add(course);
