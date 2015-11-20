4c50e7f56c84542ad5649ad3854df6a939231da7
Jason Blig <bligjason@gmail.com>
2015-11-20 14:39:55 -0600

    added comments to flowguiview

--- gowiththeflow-project/src/flowguiview/WelcomeView.java
+++ gowiththeflow-project/src/flowguiview/WelcomeView.java
@@ -3,12 +3,21 @@ package flowguiview;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class WelcomeView extends JPanel{
+/**
+ * Simple JPanel class for the "welcome" Page
+ * 
+ * @author Mostafa Dabas, Bilal Siddiqui, Danny Tsang, Jason Blig, Miguel
+ *         Cardena
+ *
+ */
+public class WelcomeView extends JPanel {
 
-	public WelcomeView(){
-		//Make this look good boyz
+	/**
+	 * constructor that creates the JLabel labeled "Welcome!"
+	 */
+	public WelcomeView() {
 		JLabel a = new JLabel("Welcome!");
 		this.add(a);
 	}
-	
+
 }
